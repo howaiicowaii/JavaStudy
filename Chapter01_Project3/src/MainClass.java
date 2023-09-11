c2df55e1574e8b8d278dd0f7038b81db473ac824
SIST <SIST@DESKTOP-9759LG3>
2023-09-11 15:37:28 +0900

    3333

--- Chapter01_Project3/src/MainClass.java
+++ Chapter01_Project3/src/MainClass.java
@@ -3,8 +3,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Java2");
-		System.out.println("Hello Java2");
+
 	}
 
 }
