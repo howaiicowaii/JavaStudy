a7b063ce3cab81038c14a87516fadfa86983e47f
SIST <SIST@DESKTOP-9759LG3>
2023-11-13 10:24:38 +0900

    231113

---
+++ OracleSelectProject_1/src/main/java/com/sist/main/EmpMain.java
@@ -0,0 +1,10 @@
+package com.sist.main;
+import com.sist.dao.*;
+public class EmpMain {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmpDAO dao=new EmpDAO();
+		dao.empListData();
+	}
+
+}
