465b56b9015c8928cbc808bb60aa0f2c55c2906d
SIST <SIST@DESKTOP-9759LG3>
2023-11-20 18:55:49 +0900

    231120

---
+++ dogCrawl/src/main/java/com/sist/vo/dogLoseVO.java
@@ -0,0 +1,50 @@
+package com.sist.vo;
+
+public class dogLoseVO {
+
+	private int dogno;
+	private String image,poster,sub,content;
+	
+	public String getPoster() {
+		return poster;
+	}
+
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getSub() {
+		return sub;
+	}
+
+	public void setSub(String sub) {
+		this.sub = sub;
+	}
+
+	public int getDogno() {
+		return dogno;
+	}
+
+	public void setDogno(int dogno) {
+		this.dogno = dogno;
+	}
+	
+	
+	
+}
