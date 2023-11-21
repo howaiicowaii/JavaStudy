4f5069ae5585c244aba2673a36d611cdb9e76af4
SIST <SIST@DESKTOP-9759LG3>
2023-11-21 17:52:06 +0900

    231121

--- dogCrawl/src/main/java/com/sist/main/dogLoseMain.java
+++ dogCrawl/src/main/java/com/sist/main/dogLoseMain.java
@@ -9,17 +9,21 @@ import org.jsoup.select.Elements;
 import com.sist.vo.dogLoseVO;
 
 public class dogLoseMain {
-
+	
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		ArrayList<String> list=new ArrayList<String>();
  
-		for(int k=2;k<=4;k++) // 도스창에 제한이 있는지 15페이지만 출력되서 혹시 몰라 2~16페이지만
+		for(int k=2;k<=3;k++) // 도스창에 제한이 있는지 15페이지만 출력되서 혹시 몰라 2~16페이지만
 		{
 		try
 		{
+				
+//			Document doc2=Jsoup.connect("http://www.angel.or.kr/index.php?code=dog").get();
+//			Elements link=doc2.select("div.wrap div.gallery a");
 			Document doc=Jsoup.connect("http://www.angel.or.kr/index.php?code=dog&page="+k+"&ski=&sci=&sco=&sgu=&q=").get();
-//			Elements link=doc.select("div.wrapper div.gallery a"); // 2~38p 상세보기들
+			Elements link=doc.select("div.wrapper div.gallery a"); // 2~38p 상세보기들
 //			System.out.println(link);
 			
 			// 첫번째 칸 (실종)
@@ -49,27 +53,14 @@ public class dogLoseMain {
 //				System.out.println(poster);
 				vo.setPoster(poster);
 				
-
-			}
-
-			
-		}catch(Exception ex) {}
-		}
-		
-		try
-		{
-			Document doc2=Jsoup.connect("http://www.angel.or.kr/index.php?code=dog").get();
-			Elements link=doc2.select("div.wrap div.gallery a");
-
-			for(int j=3;j<link.size();j++) // 1페이지 전용
-	 // j=3 으로 해놓으면 당장 1페이지에서는 20개의 상세주소 가져올 수 있다
-			{
-				String sublink=link.get(j).attr("href");
+				String sublink=link.get(i).attr("href");
 //				System.out.println(sublink);
 				String sublink2="http://www.angel.or.kr/"+sublink;
 //				System.out.println(sublink2); // sublink2 가 상세주소
 				
 				Document doc1page=Jsoup.connect(sublink2).get();
+				
+//				상세보기 - 위 제목,	정보
 				// 제목 
 				Elements titleDet=doc1page.select("div.main h2.style");
 //				System.out.println(posterDet);
@@ -81,13 +72,37 @@ public class dogLoseMain {
 				Elements infoo=doc1page.select("div.main div.about-info p.para");
 //				System.out.println(infoo);
 				String info=infoo.text();
-				System.out.println(info); // info = 글 정보 
+//				System.out.println(info); // info = 글 정보 
+				
+				
+				// 실종동물 - 상세보기 4개
+				Elements breedd=doc1page.select("table.table-hover tbody tr td");
+				String breed=breedd.text();
+				String first=breed.substring(breed.indexOf("실종동물")+5,breed.indexOf("실종날짜"));
+//				System.out.println("==================");
+//				System.out.println(first);
+				
+				String second =	breed.substring(breed.indexOf("실종날짜")+5,breed.indexOf("장소"));
+//				System.out.println(second);
+				
+				String third =	breed.substring(breed.indexOf("실종장소")+5,breed.indexOf("연락처"));
+//				System.out.println(third);
+				
+				String fourth =	breed.substring(breed.indexOf("특이사항")+5);
+//				System.out.println(fourth);
+//				System.out.println(" ====상세끝-------");
+				
 
-				
-				
-				
 			}
+
 		}catch(Exception ex) {}
+		}
 	}
+
+			
+			
+			
+				
+	
 
 }
