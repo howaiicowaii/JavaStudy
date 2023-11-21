2c196738b5a6fb5353b29d75f6f8635898d55131
SIST <SIST@DESKTOP-9759LG3>
2023-11-21 17:53:28 +0900

    231121 참고용

---
+++ dogCrawl2/src/main/java/com/sist/vo/anoLoseVO.java
@@ -0,0 +1,37 @@
+package com.sist.vo;
+
+public class anoLoseVO {
+	private int ano;
+	private String image,poster,sub,content;
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public String getSub() {
+		return sub;
+	}
+	public void setSub(String sub) {
+		this.sub = sub;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
